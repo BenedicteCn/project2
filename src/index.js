32893877b04749ce8e9a0a9c762a6b16fc459b16
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-06-12 16:14:57 +0200

    node fetch installed

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import fetch from "node-fetch";
+
+const apiUrl = "https://world.openfoodfacts.org/api/2";
+
+function getData() {
+  return fetch(apiUrl).then((response) => response.json());
+}
+
+console.log(getData());
