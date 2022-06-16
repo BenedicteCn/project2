db72ccdd71ad350a40f603ff443beed5c7baf398
Tzu Yu Chiu <tzuyujulia@gmail.com>
2022-06-16 10:38:48 +0200

    Added get recipe by name & regex on category name

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -1,11 +1,11 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const Recipe = require("../models/Recipe");
-const isAdmin = require("../middleware/isAdmin");
-const isAuthenticated = require("../middleware/isAuthenticated");
+const Recipe = require('../models/Recipe');
+const isAdmin = require('../middleware/isAdmin');
+const isAuthenticated = require('../middleware/isAuthenticated');
 
 //Get all recipes (homepage)
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const allRecipes = await Recipe.find();
     res.status(200).json(allRecipes);
@@ -13,20 +13,34 @@ router.get("/", async (req, res, next) => {
     next(err);
   }
 });
-
 // Get recipe by name
-router.get("/category/:name", async (req, res, next) => {
-  /**Your code goes here */
+router.get('/:name', async (req, res, next) => {
   try {
     const recipeName = req.params.name;
-    const allRecipes = await Recipe.find({ category: recipeName });
+
+    const OneRecipe = await Character.find({
+      name: { $regex: recipeName, $options: 'i' },
+    });
+    res.status(200).json(OneRecipe);
+  } catch (err) {
+    next(err);
+  }
+});
+// Get recipes by category
+router.get('/category/:name', async (req, res, next) => {
+  try {
+    const categoryName = req.params.name;
+    const allRecipes = await Recipe.find({
+      name: { $regex: categoryName, $options: 'i' },
+    });
+
     res.status(200).json(allRecipes);
   } catch (err) {
     next(err);
   }
 });
 
-router.delete("/:id", isAuthenticated, isAdmin, async (req, res, next) => {
+router.delete('/:id', isAuthenticated, isAdmin, async (req, res, next) => {
   try {
     const deletedThing = await Recipe.findByIdAndDelete(req.params.id);
     console.log(deletedThing);
